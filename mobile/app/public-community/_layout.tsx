f0395d3b98e97646cc9d5dd68b725c70ee2d6c81
sh1shi-q <sahanmieperera05@gmail.com>
2026-04-18 23:46:59 +0530

    feat: add _layout for page structure

---
+++ mobile/app/public-community/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+export default function PublicCommunityLayout() {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="create-post" />
+            <Stack.Screen name="post-detail" />
+        </Stack>
+    );
+}
