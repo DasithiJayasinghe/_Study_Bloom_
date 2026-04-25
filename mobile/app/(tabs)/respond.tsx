7a4308176edef884d2612779e061db2e3663e311
Ruhansi  De Silva <Ruhansisasithma@gmail.com>
2026-04-25 10:51:13 +0530

    add a tab to respond

---
+++ mobile/app/(tabs)/respond.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ChatContactsList from '@/components/chat/ChatContactsList';
+
+export default function RespondScreen() {
+    return <ChatContactsList role="responder" title="Respond" />;
+}
