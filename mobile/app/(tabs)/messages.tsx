36bc41eca6b19435fb276c8c40c811381b16616b
Ruhansi  De Silva <Ruhansisasithma@gmail.com>
2026-04-26 17:05:00 +0530

    update help request controller

---
+++ mobile/app/(tabs)/messages.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ChatContactsList from '@/components/chat/ChatContactsList';
+
+export default function MessagesScreen() {
+  return <ChatContactsList role="requester" title="Help Contacts" />;
+}
